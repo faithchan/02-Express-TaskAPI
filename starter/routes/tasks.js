89c6329f937167f710443f69d7c6f2efc9bf05c4
faithchan <fch@mercantilemaritime.com>
2022-01-02 22:54:46 +1100

    set up routes

---
+++ starter/routes/tasks.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router();
+const {getAllTasks} = require('../controllers/tasks')
+
+router.route('/').get((req,res)=>{res.send('all items')})
+
+module.exports = router
