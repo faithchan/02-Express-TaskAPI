8112c8553fec99875622dd72dbf2b4746dbf5cb0
faithchan <fch@mercantilemaritime.com>
2022-01-02 22:36:22 +1100

    second commit:

--- starter/app.js
+++ starter/app.js
@@ -2,4 +2,9 @@ const express = require('express');
 const app = express();
 const port = 5000
 
+// routes
+app.get('/hello', (req,res)=>{
+    res.send('Task Manager Initialised')
+})
+
 app.listen(port, ()=>{console.log(`port listening on port ${port}`)})
