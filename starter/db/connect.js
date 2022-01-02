14ff31de8cdc88922f3d23d26ca8ec26f959167f
faithchan <fch@mercantilemaritime.com>
2022-01-03 00:17:44 +1100

    added mongoose and edit name on url string

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -1,1 +1,5 @@
-const connectionString = "mongodb+srv://admin:<password>@cluster0.qq21j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const mongoose = require('mongoose')
+
+const connectionString = "mongodb+srv://admin:admin@cluster0.qq21j.mongodb.net/TaskManager?retryWrites=true&w=majority"
+
+mongoose.connect()
