6bb794e5d991b2e14285ead3ed2c156ef6d42ea9
faithchan <fch@mercantilemaritime.com>
2022-01-03 07:42:58 +1100

    Get Single Task

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,7 +1,9 @@
 const Task= require('../models/task')
 
-const getAllTasks = (req,res)=>{
-    res.send('all items')
+const getAllTasks = async (req,res)=>{
+    try{const tasks = await Task.find({})
+    res.status(201).json({tasks})}
+    catch(error){res.status(500).json({msg:error})}
 }
 
 const createTask = async (req,res)=>{
@@ -11,9 +13,17 @@ const createTask = async (req,res)=>{
     
 }
 
-const getTask = (req,res)=>{
-    
-    res.json({id:req.params.id})
+const getTask = async(req,res)=>{
+    try{
+        const {id: taskID} = req.params
+    const task = await Task.findOne({_id:taskID})
+    if(!task){
+        return res.status(404).json({msg:"Notask with this ID"})
+    }
+    res.status(200).json({task})
+}
+    catch(error){res.status(500).json({msg:error})}
+
 }
 
 const updateTask = (req,res)=>{
