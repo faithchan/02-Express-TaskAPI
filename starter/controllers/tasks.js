53041bc73d96fabe7fb4feb5b22a9365b15b58f2
faithchan <fch@mercantilemaritime.com>
2022-01-02 23:01:54 +1100

    basic logic of the routes set up using controllers (hardcoded)

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -2,6 +2,23 @@ const getAllTasks = (req,res)=>{
     res.send('all items')
 }
 
+const createTask = (req,res)=>{
+    res.send('Create Task')
+}
+
+const getTask = (req,res)=>{
+    res.send('Get Single Task')
+}
+
+const updateTask = (req,res)=>{
+    res.send('Update Task')
+}
+
+
+const deleteTask = (req,res)=>{
+    res.send('Delete Task')
+}
+
 module.exports={
-    getAllTasks,
+    getAllTasks, createTask, getTask, updateTask, deleteTask
 }
