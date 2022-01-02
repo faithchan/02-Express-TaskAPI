90379278de573e37ad421550e7ff16a95f4bbc4a
faithchan <fch@mercantilemaritime.com>
2022-01-03 00:18:14 +1100

    connect to db

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose')
 
 const connectionString = "mongodb+srv://admin:admin@cluster0.qq21j.mongodb.net/TaskManager?retryWrites=true&w=majority"
 
-mongoose.connect()
+mongoose.connect(connectionString)
