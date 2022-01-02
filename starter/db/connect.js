79ff614ce58380fb3c295c6f1f8118695fb7c5b4
faithchan <fch@mercantilemaritime.com>
2022-01-03 00:25:01 +1100

    remove warning on connection to db

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -3,4 +3,9 @@ const mongoose = require('mongoose')
 const connectionString = "mongodb+srv://admin:1234@cluster0.qq21j.mongodb.net/TaskManager?retryWrites=true&w=majority"
 
 // returns a promise 
-mongoose.connect(connectionString).then(()=>console.log("connected to database")).catch((err)=>console.log(err))
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }).then(()=>console.log("connected to database")).catch((err)=>console.log(err))
